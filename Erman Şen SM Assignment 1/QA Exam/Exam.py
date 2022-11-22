fee9a019c83092e9e36dc6180a7caada607ac1fa
Erman Şen <61985023+K-Rakun@users.noreply.github.com>
2022-11-22 23:01:18 +0300

    Add files via upload

---
+++ Erman Şen SM Assignment 1/QA Exam/Exam.py
@@ -0,0 +1,36 @@
+"""open a website and select the dropdown menu"""
+
+import time
+import unittest
+
+from selenium import webdriver
+from selenium.webdriver.common.by import By
+from webdriver_manager.chrome import ChromeDriverManager
+
+
+class TestHorizontalSlider(unittest.TestCase):
+    """set up"""
+    def setUp(self):
+        self.driver = webdriver.Chrome(ChromeDriverManager().install())
+        self.base_url = "http://the-internet.herokuapp.com/dropdown"
+        self.driver.maximize_window()
+        time.sleep(5)
+        self.driver.get(self.base_url)
+        time.sleep(2)
+        self.assertIn(self.base_url, self.driver.current_url)
+
+    def test_horizontal_slider(self):
+        """test the dropdown menu"""
+        self.menu = self.driver.find_element(By.ID, "dropdown")
+        self.assertTrue(self.menu)
+        self.menu.click()
+
+    def tearDown(self):
+        time.sleep(5)
+        self.driver.close()
+
+
+if __name__ == '__main__':
+    unittest.main(
+        # defaultTest="TestHorizontalSlider.test_horizontal_slider_arrow_left"
+    )
